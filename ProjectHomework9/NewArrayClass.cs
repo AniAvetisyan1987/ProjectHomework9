ec656eb12181508067d4300c87ecb59568cc102d
Armen <Armen@DESKTOP-IRSKKA4>
2023-06-20 15:43:14 +0400

    Add project files.

---
+++ ProjectHomework9/NewArrayClass.cs
@@ -0,0 +1,41 @@
+﻿namespace ProjectHomework9
+{
+    internal class NewArrayClass
+    {
+        public int[] arr;
+        public NewArrayClass(int[] arr)
+        {
+            this.arr = arr;
+        }
+
+        private int[] getNewArray()
+        {
+            int[] newArray = new int[] { };
+            for(int i = 0; i < arr.Length; i++)
+            {
+                int count = 0;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] == arr[i])
+                    {
+                        count++;
+                        if (count > 1)
+                        {
+                            
+                            if (!Array.Exists(newArray, element => element == arr[j]))
+                            {
+                                newArray = newArray.Append(arr[j]).ToArray();
+                            }
+                        }
+                    }
+                } 
+            }
+            return newArray;
+        }
+
+        public void getSum()
+        {
+            Console.WriteLine(getNewArray().Sum());
+        }
+    }
+}
---
+++ ProjectHomework9/Program.cs
@@ -0,0 +1,14 @@
+﻿using ProjectHomework9;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // ex:370
+        int[] arr = new int[] { 1, 2, 3,4,6,1,4,5,90,76,54,6,7,8,9,6,8,8,8,85,1 };
+        NewArrayClass nac = new NewArrayClass(arr);
+        nac.getSum();
+
+        Console.ReadKey();
+    }
+}
